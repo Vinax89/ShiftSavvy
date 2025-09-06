4043910f5c3bb9a1328fe31532197a914f7f9198
Irvin Bito-on <migz89star@gmail.com>
2025-09-06 02:05:19 +0000

    // src/lib/utils.ts export function cn(...xs: Array<string | number | fa

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,3 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+export function cn(...xs: Array<string | number | false | null | undefined>) {
+  return xs.filter(Boolean).join(' ')
 }
