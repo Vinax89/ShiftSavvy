854faae451e4976fcbdff0e02063e3f2e56c9cbe
Irvin Bito-on <migz89star@gmail.com>
2025-09-06 10:43:42 +0000

    content: [   './src/**/*.{js,ts,jsx,tsx,mdx}',   // keep it tight to src

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,8 @@ import type {Config} from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/**/*.{ts,tsx,mdx}',
-    './app/**/*.{ts,tsx,mdx}',
-    './components/**/*.{ts,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
