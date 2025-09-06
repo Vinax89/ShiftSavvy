1e8459c29b6b8e6b93bd8a47074d005e49009e22
Irvin Bito-on <migz89star@gmail.com>
2025-09-06 18:51:32 +0000

    export default function GlobalLoading() {   return <div className="p-6 t

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function GlobalLoading() {
+  return (
+    <div className="flex items-center justify-center h-screen w-full">
+        <div className="p-6 text-sm opacity-75">Loading…</div>
+    </div>
+  )
+}
