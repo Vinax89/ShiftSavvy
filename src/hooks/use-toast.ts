bfb31a5789f52b9a864c1956305bba676277450c
Irvin Bito-on <migz89star@gmail.com>
2025-09-06 01:31:56 +0000

    // src/components/ui/toast.ts 'use client'
    export { toast } from 'sonne

--- src/hooks/use-toast.ts
+++
@@ -1,9 +0,0 @@
-import { toast } from "sonner"
-
-// Re-export sonner's toast function to avoid refactoring all components.
-// The object is for compatibility with the previous useToast() hook signature.
-const useToast = () => {
-    return { toast };
-}
-
-export { useToast, toast }
